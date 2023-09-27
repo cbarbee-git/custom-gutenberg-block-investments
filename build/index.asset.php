767ea3ff23c84e82c20f914c70399946d7d25110
chad-idea-mktg <chad@ideamktg.com>
2023-09-27 00:45:55 -0400

    fix: remove unused icon svgs

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ecffe66d3d28bf608a30');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '10da8c818e14dc6522fc');
