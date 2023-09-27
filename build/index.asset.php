9f97e11b4afa1166e4546f35504bcb034878f184
chad-idea-mktg <chad@ideamktg.com>
2023-09-26 21:34:54 -0400

    fix: correct nesting relying on wrapper class

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e8eb70c9d13226210ccd');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4b89da2ed9d11cc85232');
